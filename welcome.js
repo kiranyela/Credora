02d6644739117d29690aff200feecf48031d1d65
kiranyela <kirenvision28@gmail.com>
2025-07-18 17:58:40 +0530

    swiper added

---
+++ welcome.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
